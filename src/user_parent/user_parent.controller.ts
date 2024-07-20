a752fb466385051959a56bd3b268cbff1a2ab38a
xJam-byte <abylaimuratbayev@gmail.com>
2024-07-20 12:25:27 +0500

    lol

--- src/user_parent/user_parent.controller.ts
+++ src/user_parent/user_parent.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
 import { UserParentService } from "./user_parent.service";
 import { UserChildeService } from "src/user_childe/user_childe.service";
 
@@ -12,5 +12,20 @@ export class UserParentController {
   @Get("/byEmail")
   async registration(@Body() email: string) {
     return this.userService.findOneByEmail(email);
+  }
+
+  @Post("/update/contacts")
+  updateNameSurnamePhone(@Body() user: any) {
+    return this.userService.updateNameSurnamePhone(user);
+  }
+
+  @Post("/update/email")
+  updateEmail(@Body() user: any) {
+    return this.userService.updateEmail(user);
+  }
+
+  @Post("/update/password")
+  updatePassword(@Body() user: any) {
+    return this.userService.updatePassword(user);
   }
 }
--- src/user_parent/user_parent.service.ts
+++ src/user_parent/user_parent.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from "@nestjs/common";
 import { UserParent } from "./user_parent.model";
 import { InjectModel } from "@nestjs/sequelize";
 import { RegisterUserDto } from "./Dto/register-user.dto";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class UserParentService {
@@ -15,7 +16,72 @@ export class UserParentService {
     return user;
   }
 
+  async getUserById(id: number) {
+    const user = await this.userModel.findOne({
+      where: { id: id },
+      include: { all: true },
+    });
+
+    return user;
+  }
+
   async createUser(createCustomerDto: RegisterUserDto): Promise<UserParent> {
     return this.userModel.create(createCustomerDto);
+  }
+
+  async updateNameSurnamePhone(user: any) {
+    const customer = await this.userModel.findOne({
+      where: { email: user.email },
+    });
+
+    if (!customer) {
+      throw new Error("Клиент с указанным email не найден.");
+    }
+    const e = await customer.update(
+      {
+        firstName: user.firstName,
+        lastName: user.lastName,
+        phoneNumber: user.phoneNumber,
+      },
+      { where: { id: customer.id } }
+    );
+
+    await customer.save();
+    return customer;
+  }
+
+  async updateEmail(user: any) {
+    const customer = await this.userModel.findOne({
+      where: { email: user.oldEmail },
+    });
+
+    if (!customer) {
+      throw new Error("Клиент с указанным email не найден.");
+    }
+    await customer.update(
+      {
+        email: user.newEmail,
+      },
+      { where: { id: customer.id } }
+    );
+
+    await customer.save();
+    return customer;
+  }
+
+  async updatePassword(user: any) {
+    const customer = await this.userModel.findOne({
+      where: { email: user.email },
+    });
+
+    if (!customer) {
+      throw new Error("Клиент с указанным email не найден.");
+    }
+
+    const hash = await bcrypt.hash(user.password, 5);
+    await customer.update({ password: hash }, { where: { id: customer.id } });
+
+    await customer.save();
+    return customer;
   }
 }
